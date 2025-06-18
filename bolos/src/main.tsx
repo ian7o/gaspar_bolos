a387b8b80da7d95db856cab0977f068185ff03f8
admin.ian.swerts <iansw.stank@gmail.com>
2025-06-18 17:20:38 +0100

    refactor: switch from HashRouter to BrowserRouter and update main route to render App component

--- bolos/src/main.tsx
+++ bolos/src/main.tsx
@@ -1,21 +1,25 @@
-import {HashRouter, Route, Routes} from "react-router-dom";
-import {createRoot} from 'react-dom/client';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router";
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 
-import './pages/styles/index.css';
-import App from './pages/App.tsx';
-import Contacts from "./pages/Contacts.tsx";
+import './pages/styles/index.css'
+import App from './pages/App.tsx'
+import Contacts from "./pages/Contacts.tsx"
 import AboutUs from "./pages/AboutUs.tsx";
 import Quiz from "./pages/Quiz.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
-        <HashRouter>
+    <StrictMode>
+        <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<Contacts/>}/>
+                <Route path={"/"} element={<App/>}/>
                 <Route path={"/home"} element={<App/>}/>
                 <Route path={"/contact"} element={<Contacts/>}/>
                 <Route path={"/about-us"} element={<AboutUs/>}/>
                 <Route path={"/quiz"} element={<Quiz/>}/>
             </Routes>
-        </HashRouter>
-);
+        </BrowserRouter>
+    </StrictMode>,
+)
