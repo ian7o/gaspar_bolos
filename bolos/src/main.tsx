f4a7a4703ec0a94d8252ce3504b05cea79802410
admin.ian.swerts <iansw.stank@gmail.com>
2025-06-18 17:29:24 +0100

    refactor: switch from HashRouter to BrowserRouter in main render

--- bolos/src/main.tsx
+++ bolos/src/main.tsx
@@ -1,15 +1,16 @@
-import {HashRouter, Route, Routes} from "react-router-dom";
-import {createRoot} from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {createRoot} from 'react-dom/client'
 
-import './pages/styles/index.css';
-import App from './pages/App.tsx';
-import Contacts from "./pages/Contacts.tsx";
+
+import './pages/styles/index.css'
+import App from './pages/App.tsx'
+import Contacts from "./pages/Contacts.tsx"
 import AboutUs from "./pages/AboutUs.tsx";
 import Quiz from "./pages/Quiz.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
             <Route path={"/"} element={<App/>}/>
             <Route path={"/home"} element={<App/>}/>
@@ -17,5 +18,5 @@ createRoot(document.getElementById('root')!).render(
             <Route path={"/about-us"} element={<AboutUs/>}/>
             <Route path={"/quiz"} element={<Quiz/>}/>
         </Routes>
-    </HashRouter>
-);
+    </BrowserRouter>
+)
