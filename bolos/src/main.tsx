265cbe404ec0a6f290655a0c4491b78fcf4dfa88
admin.ian.swerts <iansw.stank@gmail.com>
2025-06-18 15:05:46 +0100

    replace HashRouter with BrowserRouter for improved routing

--- bolos/src/main.tsx
+++ bolos/src/main.tsx
@@ -1,24 +1,24 @@
-import {HashRouter, Route, Routes} from "react-router-dom";
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router";
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 
-import './pages/styles/index.css';
-import App from './pages/App.tsx';
-import Contacts from "./pages/Contacts.tsx";
+import './pages/styles/index.css'
+import App from './pages/App.tsx'
+import Contacts from "./pages/Contacts.tsx"
 import AboutUs from "./pages/AboutUs.tsx";
 import Quiz from "./pages/Quiz.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<App/>}/>
                 <Route path={"/home"} element={<App/>}/>
                 <Route path={"/contact"} element={<Contacts/>}/>
                 <Route path={"/about-us"} element={<AboutUs/>}/>
                 <Route path={"/quiz"} element={<Quiz/>}/>
-                </Routes>
-        </HashRouter>
-    </StrictMode>
-);
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+)
