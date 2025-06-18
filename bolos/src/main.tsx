c16fd768ea561de0c878ed4c29c135dff10bd4a8
admin.ian.swerts <iansw.stank@gmail.com>
2025-06-18 15:09:49 +0100

    fix: update import path for BrowserRouter from react-router-dom

--- bolos/src/main.tsx
+++ bolos/src/main.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 
